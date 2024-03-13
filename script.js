dfb1343bba25ad707a76e51c7f51e293fe2fcb8c
umo222 <zreinasalas@gmail.com>
2024-03-12 19:21:17 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//alert("hola este es mi javascript");
+
+//let nombre = "Nicole";
+//nombre= "zarumy";
+
+// const nombre1="Nicole";
+
+//var nombre2=  "Nicole";
+
+//console.log(nombre);
+//console.log(nombre1);
+// console.log(nombre2);
+
+let contenidoTitulo="nombre"
+let titulo = document.querySelector(",logo, .fuente")
